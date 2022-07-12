6436dc4d9a220bc32d15508c7848622a77dcb1e1
Vinh <vinhbuith32412000@gmail.com>
2022-07-12 19:19:28 +0700

    Add FontAwesome

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      CONNECTED
+      
     </div>
   );
 }
