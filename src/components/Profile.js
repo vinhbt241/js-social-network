1f00e1fbe27987a4fe239eb19b5cf5df2996a219
Vinh <vinhbuith32412000@gmail.com>
2022-07-15 20:23:35 +0700

    Add static Profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,32 @@
-import { Sidebar } from "./Sidebar"
+import { Sidebar } from "./Sidebar";
+import { UserInfo } from "./UserInfo";
+import { Post } from "./Post";
 
 const Profile = (props) => {
   return(
     <div className="Profile">
       <Sidebar />
+      <div class="user-img-section">
+        <img src={props.userBackgroundImage} alt="USER BACKGROUND IMG" className="user-bg-img"/>
+        <UserInfo userName="Current User Name" classToAdd="user-info-in-profile"/>
+      </div>
+
+      <ul className="profile-post-container">
+        <li>
+          <Post
+           userName="Your Post 1"
+           description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat sed corporis voluptate recusandae, doloribus velit at harum voluptas labore eum, aut alias? Amet, debitis nam. Consequuntur est qui sunt quis. "
+           numLikes="312"
+           numComments="777"/>
+        </li>
+        <li>
+          <Post
+           userName="Share Post 1"
+           description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat sed corporis voluptate recusandae, doloribus velit at harum voluptas labore eum, aut alias? Amet, debitis nam. Consequuntur est qui sunt quis. "
+           numLikes="312"
+           numComments="777"/>
+        </li>
+      </ul>    
     </div>
   )
 }
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,7 +1,7 @@
 const UserInfo = (props) => {
   return(
     <div className={`UserInfo ${props.classToAdd}`}>
-      <img src={props.userImg} alt=""/>
+      <img src={props.userImg} alt="USER INFO"/>
       <h1>{props.userName}</h1>
     </div>
   )
