c96ef32046c7de817d01ce398d103cf141133c54
Vinh <vinhbuith32412000@gmail.com>
2022-07-16 21:22:03 +0700

    Fix wrong attribute name

--- src/components/Associates.js
+++ src/components/Associates.js
@@ -5,22 +5,22 @@ const Associates = (props) => {
     <div className="Associates">
       <h1>Associates</h1>
 
-      <div class="friend-status">
+      <div className="friend-status">
         <UserInfo userImg={props.userImg} userName="Friend 1" classToAdd="small-font"/>
         <span className="online-status is-online"></span>
       </div>
 
-      <div class="friend-status">
+      <div className="friend-status">
         <UserInfo userImg={props.userImg} userName="Friend 2" classToAdd="small-font"/>
         <span className="online-status"></span>
       </div>
 
-      <div class="friend-status">
+      <div className="friend-status">
         <UserInfo userImg={props.userImg} userName="Friend 3" classToAdd="small-font"/>
         <span className="online-status is-online"></span>
       </div>
 
-      <div class="friend-status">
+      <div className="friend-status">
         <UserInfo userImg={props.userImg} userName="Friend 4" classToAdd="small-font"/>
         <span className="online-status"></span>
       </div>
