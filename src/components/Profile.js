b23fa72c70a8485528cd2d235da44a120ec99307
Vinh <vinhbuith32412000@gmail.com>
2022-07-16 20:55:37 +0700

    Refactor profile page to retrieve and display user's info and user's post from backend

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,31 +1,59 @@
 import { Sidebar } from "./Sidebar";
 import { UserInfo } from "./UserInfo";
 import { Post } from "./Post";
+import { useEffect, useState } from "react";
+
+const USER_API_URL = "http://127.0.0.1:3000/api/users/1"
+const USER_POST_API_URL = "http://127.0.0.1:3000/api/posts/1"
 
 const Profile = (props) => {
+  const [userInfo, setUserInfo] = useState("Loading...");
+  const [userPosts, setUserPosts] = useState([]);
+
+  const getUserInfo = async () => {
+    const response = await fetch(USER_API_URL);
+    const data = await response.json();
+    return data
+  }
+
+  const getUserPost = async () => {
+    const response = await fetch(USER_POST_API_URL);
+    const data = await response.json();
+    return data; 
+  }
+
+  useEffect(() => {
+    getUserInfo().then(userData => {
+      setUserInfo(userData);
+    })
+
+    getUserPost().then(postsData => {
+      setUserPosts(postsData);
+    })
+  },[])
+
+  const listPosts = userPosts.map(post => {
+    return(
+      <li key={post.id}>
+        <Post 
+          userName={post.user.name}
+          description={post.content} 
+          numLikes="0" 
+          numComments="0" />
+      </li>
+    )
+  })
+
   return(
     <div className="Profile">
       <Sidebar />
-      <div class="user-img-section">
+      <div className="user-img-section">
         <img src={props.userBackgroundImage} alt="USER BACKGROUND IMG" className="user-bg-img"/>
-        <UserInfo userName="Current User Name" classToAdd="user-info-in-profile"/>
+        <UserInfo userName={userInfo.name} classToAdd="user-info-in-profile"/>
       </div>
 
       <ul className="profile-post-container">
-        <li>
-          <Post
-           userName="Your Post 1"
-           description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat sed corporis voluptate recusandae, doloribus velit at harum voluptas labore eum, aut alias? Amet, debitis nam. Consequuntur est qui sunt quis. "
-           numLikes="312"
-           numComments="777"/>
-        </li>
-        <li>
-          <Post
-           userName="Share Post 1"
-           description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat sed corporis voluptate recusandae, doloribus velit at harum voluptas labore eum, aut alias? Amet, debitis nam. Consequuntur est qui sunt quis. "
-           numLikes="312"
-           numComments="777"/>
-        </li>
+        {listPosts}
       </ul>    
     </div>
   )
