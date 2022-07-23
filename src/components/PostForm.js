63b0c2206939cd6161d964de98abb99d26e8089b
Vinh <vinhbuith32412000@gmail.com>
2022-07-23 20:22:34 +0700

    add style to post form, but not complete because laptop's monitor is broken

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -3,14 +3,13 @@ import { UserInfo } from "./UserInfo"
 const PostForm = (props) => {
   return(
     <form className="PostForm">
-      <div>
+      <div className="PostForm-content">
         <UserInfo user={props.currentUser} hideName={true}/>
         <input
           type="text" name="content"
-          placeholder="Share what you're thinking"
-          className="PostForm-content-input"/>
+          placeholder="Share what you're thinking"/>
       </div>
-      <input type="file" name="image" />
+      <input type="file" name="image"/>
       <input type="hidden" name="user_id" value={props.currentUser.id} />
       <input type="submit" value="post" />
     </form>
