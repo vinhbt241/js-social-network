7c5c2bd01e9311c2d50e3571d8670e8c8ee9b894
Vinh <vinhbuith32412000@gmail.com>
2022-07-28 20:08:57 +0700

    Add state and command to retrieve user's potential friends and pending friends

--- src/components/FriendRequests.js
+++ src/components/FriendRequests.js
@@ -1,69 +1,59 @@
 import { FriendInvite } from "./FriendInvite"
 import { AddFriendRequest } from "./AddFriendRequest"
 import { Sidebar } from "./Sidebar"
+import { useEffect, useState } from "react";
+import { getData } from "../utilities";
 
 const FriendRequests = (props) => {
+  const currentUser = JSON.parse(localStorage.user);
+
+  const PENDING_FRIENDS_API_URL = `http://127.0.0.1:3000/api/users/${currentUser.id}/pending_friends`;
+  const POTENTIAL_FRIENDS_API_URL = `http://127.0.0.1:3000/api/users/${currentUser.id}/potential_friends`;
+
+  const [pendingFriends, setPendingFriends] = useState([]);
+  const [potentialFriends, setPotentialFriends] = useState([]);
+
+  useEffect(() => {
+    getData(PENDING_FRIENDS_API_URL).then(pendingFriendsData => {
+      setPendingFriends(pendingFriendsData)
+    })
+
+    getData(POTENTIAL_FRIENDS_API_URL).then(potentialFriendsData => {
+      setPotentialFriends(potentialFriendsData)
+    })
+  }, [])
+
+  const listPendingFriends = pendingFriends.map(pendingFriend => {
+    return(
+      <li key={pendingFriend.id}>
+        <FriendInvite user={pendingFriend}/>
+      </li>
+    )
+  }) 
+
+  const listPotentialFriends = potentialFriends.map(potentialFriend => {
+    return(
+      <li key={potentialFriend.id}>
+        <AddFriendRequest user={potentialFriend}/>
+      </li>
+    )
+  }) 
+
   return(
     <div className="FriendRequests normal-page">
       <Sidebar />
 
       <div>
-        <h1>Friend Invites</h1>
+        <h1>Pending Friend Requests</h1>
         <ul className="request-container">
-          <li>
-            <FriendInvite user={{name: "User 1"}}/>
-          </li>
-          <li>
-            <FriendInvite user={{name: "User 1"}}/>
-          </li>
-          <li>
-            <FriendInvite user={{name: "User 1"}}/>
-          </li>
-          <li>
-            <FriendInvite user={{name: "User 1"}}/>
-          </li>
-          <li>
-            <FriendInvite user={{name: "User 1"}}/>
-          </li>
-          <li>
-            <FriendInvite user={{name: "User 1"}}/>
-          </li>
-          <li>
-            <FriendInvite user={{name: "User 1"}}/>
-          </li>
-          <li>
-            <FriendInvite user={{name: "User 1"}}/>
-          </li>
+          {listPendingFriends}
         </ul>
       </div>
 
       <div>
         <h1>Let's know each other!</h1>
         <ul className="request-container">
-          <li>
-            <AddFriendRequest user={{name: "User 1"}}/>
-          </li>
-          <li>
-            <AddFriendRequest user={{name: "User 1"}}/>
-          </li>
-          <li>
-            <AddFriendRequest user={{name: "User 1"}}/>
-          </li>
-          <li>
-            <AddFriendRequest user={{name: "User 1"}}/>
-          </li>
-          <li>
-            <AddFriendRequest user={{name: "User 1"}}/>
-          </li>
-          <li>
-            <AddFriendRequest user={{name: "User 1"}}/>
-          </li>
-          <li>
-            <AddFriendRequest user={{name: "User 1"}}/>
-          </li>
-          <li>
-            <AddFriendRequest user={{name: "User 1"}}/>
-          </li>
+          {listPotentialFriends}
         </ul>
       </div>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ import { PostForm } from "./PostForm";
 import { Sidebar } from "./Sidebar"
 
 const Home = () => {
-  let currentUser = JSON.parse(localStorage.user)
+  const currentUser = JSON.parse(localStorage.user)
 
   const FRIENDS_API_URL = `http://127.0.0.1:3000/api/users/${currentUser.id}/friends`;
   const POSTS_API_URL = `http://127.0.0.1:3000/api/users/${currentUser.id}/user_and_friends_posts`;
