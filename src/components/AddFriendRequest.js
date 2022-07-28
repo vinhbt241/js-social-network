d7bf41163da3a7595fcf60c4d4f6a6fe60c8e29b
Vinh <vinhbuith32412000@gmail.com>
2022-07-28 22:34:04 +0700

    Add UI to allow user to send as well as decline friend request

--- src/components/AddFriendRequest.js
+++ src/components/AddFriendRequest.js
@@ -1,11 +1,40 @@
-import { UserInfo } from "./UserInfo"
+import { useState } from "react";
+import { UserInfo } from "./UserInfo";
 
 const AddFriendRequest = (props) => {
+  const currentUser = JSON.parse(localStorage.user);
+
+  const SEND_FRIEND_REQUEST_API_URL = "http://127.0.0.1:3000/api/users/send_friend_request";
+
+  const [requestSent, setRequestSent] = useState(false);
+
+  const sendFriendRequest = async () => {
+    await fetch(SEND_FRIEND_REQUEST_API_URL, {
+      method: "POST",
+      body: JSON.stringify({
+        user_id: currentUser.id,
+        friend_id: props.user.id
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        Accept: "application/json",
+        Authorization: localStorage.token
+      }
+    })
+
+    setRequestSent(true);
+  }
+
   return(
     <div className="FriendRequest request-box">
       <UserInfo user={props.user} classToAdd="user-info-in-request-box"/>
-      <button className="primary-color-bg">Add friend</button>
-      <button>Delete</button>
+      <button 
+        className="primary-color-bg" 
+        onClick={sendFriendRequest}
+        disabled={requestSent}>
+        {requestSent ? "Request sent" : "Add Friend"}
+      </button>
+      <button disabled={requestSent}>Delete</button>
     </div>
   )
 }
--- src/components/FriendInvite.js
+++ src/components/FriendInvite.js
@@ -1,11 +1,64 @@
+import { useState } from "react";
 import { UserInfo } from "./UserInfo"
 
 const FriendInvite = (props) => {
+  const currentUser = JSON.parse(localStorage.user);
+
+  const ACCEPT_PENDING_REQUEST_API_URL = "http://127.0.0.1:3000/api/users/accept_pending_request";
+  const DECLINE_FRIEND_REQUEST_API_URL = "http://127.0.0.1:3000/api/users/decline_friend_request";
+
+  const [acceptSent, setAcceptSent] = useState(false);
+  const [declineSent, setDeclineSent] = useState(false);
+
+  const sendAcceptRequest = async () => {
+    await fetch(ACCEPT_PENDING_REQUEST_API_URL, {
+      method: "POST",
+      body: JSON.stringify({
+        user_id: currentUser.id,
+        friend_id: props.user.id
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        Accept: "application/json",
+        Authorization: localStorage.token
+      }
+    })
+
+    setAcceptSent(true);
+  }
+
+  const sendDeclineRequest = async () => {
+    await fetch(DECLINE_FRIEND_REQUEST_API_URL, {
+      method: "POST",
+      body: JSON.stringify({
+        user_id: currentUser.id,
+        friend_id: props.user.id
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        Accept: "application/json",
+        Authorization: localStorage.token
+      }
+    })
+
+    setDeclineSent(true);
+  }
+
+
   return(
     <div className="FriendInvite request-box">
       <UserInfo user={props.user} classToAdd="user-info-in-request-box"/>
-      <button className="primary-color-bg">Accept</button>
-      <button>Delete</button>
+      <button 
+        className="primary-color-bg" 
+        onClick={sendAcceptRequest}
+        disabled={acceptSent || declineSent}>
+        {acceptSent ? "Accepted" : "Accept"}
+      </button>
+      <button 
+        onClick={sendDeclineRequest}
+        disabled={acceptSent || declineSent}>
+        {declineSent ? "Declined" : "Decline"}
+      </button>
     </div>
   )
 }
