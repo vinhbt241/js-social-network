6d14d720e7ea854e77977f235a8707f708b9f9c7
Vinh <vinhbuith32412000@gmail.com>
2022-07-30 15:05:28 +0700

    Add full static setting page

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -1,37 +1,77 @@
-import { Sidebar } from "./Sidebar"
+import { useState } from "react";
+import { Sidebar } from "./Sidebar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { faKey } from "@fortawesome/free-solid-svg-icons";
+import { faUserEdit } from "@fortawesome/free-solid-svg-icons";
 
 const Setting = () => {
   const currentUser = JSON.parse(localStorage.user);
+
+  const [displayProfileUpdate, setDisplayProfileUpdate] = useState(false);
+  const [displayAccountUpdate, setDisplayAccountUpdate] = useState(false);
 
   return(
     <div className="Setting normal-page">
       <Sidebar />
 
-      <button>
+      <h1>
+        Settings
+      </h1>
+
+      <button className="toggle-update-display" onClick={() => setDisplayProfileUpdate(true)}>
         Update profile
       </button>
 
-      <button>
+      <button className="toggle-update-display" onClick={() => setDisplayAccountUpdate(true)}>
         Update account
       </button>
 
-      <div className="user-profile-display">
-        <h1>Update profile</h1>
+      {
+        displayProfileUpdate &&
+        <div className="fixed-background">
+          <div className="user-profile-display">
+            <h1>Update profile</h1>
+            <div className="update-section">
+              <h1>Avatar</h1>
+              <button className="unstyle-button update-button">Update</button>
+            </div>
+            <img src={currentUser.avatar_url} alt="" className="user-avatar-in-settings"/>
+            <div className="update-section">
+              <h1>Background Image</h1>
+              <button className="unstyle-button update-button">Update</button>
+            </div>
+            <img src={currentUser.background_image_url} alt="" className="user-bg-img-in-settings"/>
 
-        <div className="update-profile-section">
-          <h2>Avatar</h2>
-          <button>Update</button>
+            <button className="close-form-button" onClick={() => setDisplayProfileUpdate(false)}>
+              <FontAwesomeIcon icon={faClose}/>
+            </button>
+          </div>
         </div>
+      }
 
-        <img src={currentUser.avatar_url} alt="" className="user-avatar-in-settings"/>
+      {
+        displayAccountUpdate &&
+        <div className="fixed-background">
+          <div className="user-account-display">
+            <h1>Update Account</h1>
 
-        <div className="update-profile-section">
-          <h2>Background Image</h2>
-          <button>Update</button>
+            <div className="update-section">
+              <h1><FontAwesomeIcon icon={faUserEdit}/> Change username</h1>
+              <button className="unstyle-button update-button">Update</button>
+            </div>
+
+            <div className="update-section">
+              <h1><FontAwesomeIcon icon={faKey}/> Change password</h1>
+              <button className="unstyle-button update-button">Update</button>
+            </div>
+
+            <button className="close-form-button" onClick={() => setDisplayAccountUpdate(false)}>
+              <FontAwesomeIcon icon={faClose}/>
+            </button>
+          </div>
         </div>
-
-        <img src={currentUser.background_image_url} alt="" className="user-bg-img-in-settings"/>
-      </div>
+      }
     </div>  
   )
 }
