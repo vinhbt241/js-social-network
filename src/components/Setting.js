796846e720951e10923ab52204556a101bedeb9b
Vinh <vinhbuith32412000@gmail.com>
2022-07-30 20:38:44 +0700

    Complete static form for user to update their image as well as account

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -4,12 +4,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
 import { faKey } from "@fortawesome/free-solid-svg-icons";
 import { faUserEdit } from "@fortawesome/free-solid-svg-icons";
+import { faImage } from "@fortawesome/free-solid-svg-icons";
 
 const Setting = () => {
   const currentUser = JSON.parse(localStorage.user);
 
   const [displayProfileUpdate, setDisplayProfileUpdate] = useState(false);
   const [displayAccountUpdate, setDisplayAccountUpdate] = useState(false);
+  const [displayUpdateNameForm, setDisplayUpdateNameForm] = useState(false);
+  const [displayUpdatePasswordForm, setDisplayUpdatePasswordForm] = useState(false);
+  const [displayUpdateAvatarForm, setDisplayUpdateAvatarForm] = useState(false);
+  const [displayUpdateBgImgForm, setDisplayUpdateBgImgForm] = useState(false);
 
   return(
     <div className="Setting normal-page">
@@ -30,16 +35,26 @@ const Setting = () => {
       {
         displayProfileUpdate &&
         <div className="fixed-background">
-          <div className="user-profile-display">
+          <div className="display-container">
             <h1>Update profile</h1>
             <div className="update-section">
               <h1>Avatar</h1>
-              <button className="unstyle-button update-button">Update</button>
+              <button 
+                className="unstyle-button update-button"
+                onClick={() => {
+                  setDisplayUpdateAvatarForm(true);
+                  setDisplayProfileUpdate(false);
+                }}>Update</button>
             </div>
             <img src={currentUser.avatar_url} alt="" className="user-avatar-in-settings"/>
             <div className="update-section">
               <h1>Background Image</h1>
-              <button className="unstyle-button update-button">Update</button>
+              <button 
+                className="unstyle-button update-button"
+                onClick={() => {
+                  setDisplayUpdateBgImgForm(true);
+                  setDisplayProfileUpdate(false);
+                }}>Update</button>
             </div>
             <img src={currentUser.background_image_url} alt="" className="user-bg-img-in-settings"/>
 
@@ -48,22 +63,37 @@ const Setting = () => {
             </button>
           </div>
         </div>
+        
       }
-
+      
       {
         displayAccountUpdate &&
         <div className="fixed-background">
-          <div className="user-account-display">
+          <div className="display-container">
             <h1>Update Account</h1>
 
             <div className="update-section">
               <h1><FontAwesomeIcon icon={faUserEdit}/> Change username</h1>
-              <button className="unstyle-button update-button">Update</button>
+              <button 
+                className="unstyle-button update-button"
+                onClick={() => {
+                  setDisplayAccountUpdate(false);
+                  setDisplayUpdateNameForm(true);
+                }}>
+                Update
+              </button>
             </div>
 
             <div className="update-section">
               <h1><FontAwesomeIcon icon={faKey}/> Change password</h1>
-              <button className="unstyle-button update-button">Update</button>
+              <button 
+                className="unstyle-button update-button"
+                onClick={() => {
+                  setDisplayAccountUpdate(false);
+                  setDisplayUpdatePasswordForm(true);
+                }}>
+                Update
+              </button>
             </div>
 
             <button className="close-form-button" onClick={() => setDisplayAccountUpdate(false)}>
@@ -72,6 +102,114 @@ const Setting = () => {
           </div>
         </div>
       }
+
+      {
+        displayUpdateNameForm &&
+        <div className="fixed-background">
+          <form className="display-container">
+
+            <label className="update-form-label">
+              Update name
+              <input className="update-form-input" type="text" name="name" required />
+            </label>
+
+            <input className="update-form-submit" type="submit" value="Change" />
+
+            <button 
+              className="close-form-button" onClick={ () => {
+                setDisplayUpdateNameForm(false);
+                setDisplayAccountUpdate(true);
+              }}>
+                <FontAwesomeIcon icon={faClose}/>
+            </button>
+          </form>
+        </div>
+      }
+
+      {
+        displayUpdatePasswordForm &&
+        <div className="fixed-background">
+          <form className="display-container">
+            <label className="update-form-label">
+              New password
+              <input className="update-form-input" type="password" name="password" required/>
+
+            </label>
+            <label className="update-form-label">
+              Confirm password
+              <input className="update-form-input" type="password" name="password-confirmation" required/>
+            </label>
+
+            <input className="update-form-submit" type="submit" value="Change" />
+
+            <button 
+                className="close-form-button" onClick={ () => {
+                setDisplayUpdatePasswordForm(false);
+                setDisplayAccountUpdate(true);
+              }}>
+                <FontAwesomeIcon icon={faClose}/>
+            </button>
+          </form>
+        </div>
+      }
+
+      {
+        displayUpdateAvatarForm &&
+        <div className="fixed-background">
+          <form className="display-container">
+            <strong className="update-form-name">Update avatar</strong>
+            <label className="update-form-label">
+              <div className="update-image-click">
+                <FontAwesomeIcon icon={faImage} /> Choose Image
+              </div>
+
+              <input type="file" className="hidden-input" />
+            </label>
+
+            <img src={currentUser.avatar_url} alt="" className="user-avatar-in-settings"/>
+
+            <input className="update-form-submit" type="submit" value="Change" />
+
+            <button 
+                className="close-form-button" onClick={ () => {
+                  setDisplayUpdateAvatarForm(false);
+                  setDisplayProfileUpdate(true);
+                }}>
+                  <FontAwesomeIcon icon={faClose}/>
+            </button>
+          </form>
+        </div>
+      }
+
+      {
+        displayUpdateBgImgForm &&
+        <div className="fixed-background">
+          <form className="display-container">
+            <strong className="update-form-name">Update background image</strong>
+            <label className="update-form-label">
+              <div className="update-image-click">
+                <FontAwesomeIcon icon={faImage} /> Choose Image
+              </div>
+
+              <input type="file" className="hidden-input" />
+            </label>
+
+            <img src={currentUser.background_image_url} alt="" className="user-bg-img-in-settings"/>
+
+            <input className="update-form-submit" type="submit" value="Change" />
+
+            <button 
+                className="close-form-button" onClick={ () => {
+                  setDisplayUpdateBgImgForm(false);
+                  setDisplayProfileUpdate(true);
+                }}>
+                  <FontAwesomeIcon icon={faClose}/>
+            </button>
+          </form>
+        </div>
+      }
+
+      
     </div>  
   )
 }
