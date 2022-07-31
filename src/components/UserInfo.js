c91fa0f95a67c918d7163b5e44d4f919dc74fb34
Vinh <vinhbuith32412000@gmail.com>
2022-07-31 14:43:34 +0700

    Change default user avatar

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom"
+import DefaultAvatar from "../images/default_avatar.jpg"
 
 const UserInfo = (props) => {
   return(
     <div className={`UserInfo ${props.classToAdd}`}>
       <Link to={`/profile/${props.user.id}`} className="unstyle-link">
-        <img src={props.user.avatar_url} alt=""/>
+        <img src={props.user.avatar_url || DefaultAvatar} alt=""/>
       </Link>
       <Link to={`/profile/${props.user.id}`} className="unstyle-link">
         {props.hideName ? "" : <h1>{props.user.name}</h1>}
